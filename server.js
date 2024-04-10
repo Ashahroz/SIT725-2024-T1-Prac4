680506e3e321bce5f2196b1aa1e0dd30f2bb924c
Ashahroz <127468075+Ashahroz@users.noreply.github.com>
2024-04-10 12:01:39 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var app = express();
+let projectCollection;
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://aimanshahroz94:4jMdbDyn28DLRfqz@cluster0.qav2cmu.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+function connect() {
+    client.connect(err => {
+
+        if (err) console.log(err);
+        else {
+            projectCollection = client.db("test").collection("Cats");
+        };
+    });
+}
+
+const insertProjects = (project, callback) => {
+    projectCollection.insertOne(project, callback);
+};
+
+const getProjects = (callback) => {
+    projectCollection.find({}).toArray(callback);
+};
+
+app.post('/api/projects', (req, res) => {
+    let newProject = req.body;
+    insertProjects(newProject, (error, result) => {
+        if (error) {
+            res.json({ statusCode: 400, message: error });
+        } else {
+            res.json({ statusCode: 200, data: result, message: 'successfully added' });
+        }
+    });
+});
+
+app.get('/api/projects', (req, res) => {
+    getProjects((error, result) => {
+        if (error) {
+            res.json({ statusCode: 400, message: error });
+        } else {
+            res.json({ statusCode: 200, data: result, message: 'Success' });
+        }
+    });
+});
+
+
+var port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log('App listening to: ' + port);
+    connect();
+});
